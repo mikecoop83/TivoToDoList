940fac9222e3d2778dbded57f46648fd037c8b53
Michael Coopersmith <mikecoop83@gmail.com>
2017-11-21 23:25:06 -0500

    building amd64 and arm64

--- build.sh
+++ build.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
-go build -o ./bin/TivoToDoList src/*.go
+echo "Building arm64..."
+GOARCH=arm64 go build -o ./bin/arm64/TivoToDoList src/*.go
+echo "Building amd64..."
+GOARCH=amd64 go build -o ./bin/amd64/TivoToDoList src/*.go
+
 cp src/TivoToDoList.conf bin/
 
