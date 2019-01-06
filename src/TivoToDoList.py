9540527a9a89282c804dbbc01f9261f34de54efa
Michael Coopersmith <mikecoop83@gmail.com>
2019-01-05 20:59:27 -0500

    using tvmaze for shows not on tivo

--- src/TivoToDoList.py
+++ src/TivoToDoList.py
@@ -2,8 +2,10 @@
 import json
 import logging
 import os
+import requests
 import smtplib
 import sys
+from bs4 import BeautifulSoup
 from datetime import date, datetime, timedelta
 from email.mime.text import MIMEText
 from typing import List, Dict
@@ -22,16 +24,33 @@ def local_datetime_from_utc_string(str_utc_time: str) -> datetime:
 
 
 class EpisodeDetails:
-    def __init__(self, ep: Dict):
-        self.title = ep.get("title")
-        self.subtitle = ep.get("subtitle")
-        self.description = ep.get("description")
-        self.requested_start_time = local_datetime_from_utc_string(
+    @classmethod
+    def from_tivo_dict(cls, ep: Dict):
+        ed = EpisodeDetails()
+        ed.title = ep.get("title")
+        ed.subtitle = ep.get("subtitle")
+        ed.description = ep.get("description")
+        ed.requested_start_time = local_datetime_from_utc_string(
             ep.get("requestedStartTime")
         )
-        self.requested_end_time = local_datetime_from_utc_string(
+        ed.requested_end_time = local_datetime_from_utc_string(
             ep.get("requestedEndTime")
         )
+        return ed
+
+    @classmethod
+    def from_tvmaze_dict(cls, ep: Dict):
+        ed = EpisodeDetails()
+        ed.title = ep["show"]["name"]
+        ed.subtitle = ep["name"]
+        ed.description = ep["show"]["summary"]
+        if ed.description:
+            ed.description = BeautifulSoup(ed.description, "html.parser").get_text()
+        ed.requested_start_time = local_datetime_from_utc_string(ep["airstamp"])
+        ed.requested_end_time = ed.requested_start_time + timedelta(
+            minutes=ep["runtime"]
+        )
+        return ed
 
     def to_html(self) -> str:
         subtitle = self.subtitle or self.description or "Unknown"
@@ -40,13 +59,17 @@ class EpisodeDetails:
 
 
 if __name__ == "__main__":
-    logging.basicConfig(format='%(asctime)s [%(levelname)s] %(message)s', level=logging.DEBUG)
+    logging.basicConfig(
+        format="%(asctime)s [%(levelname)s] %(message)s", level=logging.DEBUG
+    )
     config_filename = "TivoToDoList.conf"
     logging.debug(f"Loading config file from {os.path.abspath(config_filename)}")
     with open(config_filename, "r") as configFile:
         config = json.loads(configFile.read())
 
-    logging.info(f"Connecting to tivo at {config['tivo_ip']}:{config['tivo_port']} using cert at {os.path.abspath(config['cert_path'])}")
+    logging.info(
+        f"Connecting to tivo at {config['tivo_ip']}:{config['tivo_port']} using cert at {os.path.abspath(config['cert_path'])}"
+    )
     mind = api.Mind.new_local_session(
         cert_path=config["cert_path"],
         cert_password=config["cert_password"],
@@ -57,22 +80,38 @@ if __name__ == "__main__":
 
     logging.info(f"Querying tivo for to do list")
     to_do_list = mind.recording_search(
-        fetch_all=True, filt={
-            "state": ["inProgress", "scheduled"]
-        }
+        fetch_all=True, filt={"state": ["inProgress", "scheduled"]}
     )
 
+    today = datetime.today().date()
+    dates_and_labels = [(today, "Today"), (today + timedelta(days=1), "Tomorrow")]
+
     logging.debug("Finding new episodes")
-    new_eps = [EpisodeDetails(ep) for ep in to_do_list if ep["isNew"]]
+    new_eps = [EpisodeDetails.from_tivo_dict(ep) for ep in to_do_list if ep["isNew"]]
+
+    tvmaze_eps = []
+    try:
+        tvmaze_show_ids = set(config.get("tvmaze_show_ids", []))
+        if tvmaze_show_ids:
+            logging.info(f"Querying tvmaze for schedule for {dates_and_labels}")
+            for date, _ in dates_and_labels:
+                tvmaze_url = f"http://api.tvmaze.com/schedule?date={date}"
+                guide = requests.get(tvmaze_url).json()
+                new_eps += [
+                    EpisodeDetails.from_tvmaze_dict(ep)
+                    for ep in guide
+                    if ep.get("show", {}).get("id") in tvmaze_show_ids
+                ]
+    except Exception:
+        logging.exception("Error querying tvmaze for schedule")
 
     def get_new_eps_by_date(start_date: date) -> List[EpisodeDetails]:
         result = [ep for ep in new_eps if ep.requested_start_time.date() == start_date]
         result.sort(key=lambda ep: ep.requested_start_time)
         return result
 
-    today = datetime.today().date()
     message_list = []
-    for date, label in [(today, "Today"), (today + timedelta(days=1), "Tomorrow")]:
+    for date, label in dates_and_labels:
         message_list += [f"{label}'s new episodes:"] + [
             ep.to_html() for ep in get_new_eps_by_date(date)
         ]
@@ -94,7 +133,7 @@ if __name__ == "__main__":
         smtp_client.starttls()
         smtp_client.login(config["smtp_user"], config["smtp_password"])
         smtp_client.sendmail(
-            config["smtp_user"], config["smtp_user"], mime_message.as_string()
+            config["smtp_user"], config["to_emails"], mime_message.as_string()
         )
         smtp_client.close()
 
